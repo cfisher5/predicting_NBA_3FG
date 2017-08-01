b58f889a5267c3576aeee18da99c2a62836b1ac7
Cole Fisher <cfishe29@uwo.ca>
2017-08-01 15:22:33 -0400

    small edits + report file

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import regression
 import statistics
 import pandas as pd
 
-
 # Scrape data
 # scraper.sports_ref_scrape("https://www.sports-reference.com/cbb/play-index/psl_finder.cgi?request=1&match=combined&year_min=2002&year_max=2016&conf_id=&school_id=&class_is_fr=Y&class_is_so=Y&class_is_jr=Y&class_is_sr=Y&pos_is_g=Y&pos_is_gf=Y&pos_is_fg=Y&pos_is_f=Y&pos_is_fc=Y&pos_is_cf=Y&pos_is_c=Y&games_type=A&qual=&c1stat=g&c1comp=gt&c1val=20&c2stat=fg3a_per_g&c2comp=gt&c2val=1.5&c3stat=&c3comp=&c3val=&c4stat=&c4comp=&c4val=&order_by=pts&order_by_asc=&offset=", "data/july25ncaa.csv")
 # scraper.sports_ref_scrape("https://www.basketball-reference.com/play-index/psl_finder.cgi?request=1&match=combined&type=totals&per_minute_base=36&per_poss_base=100&lg_id=NBA&is_playoffs=N&year_min=2003&year_max=2017&franch_id=&season_start=1&season_end=-1&age_min=0&age_max=99&shoot_hand=&height_min=0&height_max=99&birth_country_is=Y&birth_country=&birth_state=&college_id=&draft_year=&is_active=&debut_yr_nba_start=&debut_yr_nba_end=&is_hof=&is_as=&as_comp=gt&as_val=0&award=&pos_is_g=Y&pos_is_gf=Y&pos_is_f=Y&pos_is_fg=Y&pos_is_fc=Y&pos_is_c=Y&pos_is_cf=Y&qual=&c1stat=g&c1comp=gt&c1val=50&c2stat=mp_per_g&c2comp=gt&c2val=7&c3stat=fg3a&c3comp=gt&c3val=150&c4stat=&c4comp=&c4val=&c5stat=&c5comp=&c6mult=&c6stat=&order_by=fg3&order_by_asc=&offset=", "data/july25nba.csv")
